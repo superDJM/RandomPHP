6bc87dca71ede7ed119a5113fb812c1a95d9c348
op87960 <op87960@gmail.com>
2016-03-24 20:25:40 +0800

    修复路由中没有参数却array_pop的问题

--- Library/Random/Router.php
+++ Library/Random/Router.php
@@ -19,8 +19,8 @@ class Router
     {
         $param = Array();
         //        $uri = $_SERVER['SCRIPT_NAME'];
-        $uri = $_SERVER['PATH_INFO'];   //使用path_info模式
-        if (empty($uri)) {
+        $uri = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';   //使用path_info模式
+        if (!isset($_SERVER['PATH_INFO']) && !DEBUG) {
             throw new \Exception("系统不支持path_info");
         }
 
@@ -40,16 +40,15 @@ class Router
         //不规范的url,统一定位到Home/Home/index
         if (count($request) < 3) {
             return array('Home', 'Home', 'index');
+        } elseif (count($request) > 3) {
+            //get Param
+            preg_replace_callback('/([^\/]+)\/([^\/]+)/', function ($match) use (&$param) {
+                $param[strtolower($match[1])] = strip_tags($match[2]);
+            }, array_pop($request));
+
+            //把param加入到$_GET
+            $_GET = array_merge($param, $_GET);
         }
-
-        //get Param
-        preg_replace_callback('/([^\/]+)\/([^\/]+)/', function ($match) use (&$param) {
-            $param[strtolower($match[1])] = strip_tags($match[2]);
-        }, array_pop($request));
-
-        //把param加入到$_GET
-        $_GET = array_merge($param, $_GET);
-
 
         //get Module
         $router[] = ucwords(array_shift($request));
