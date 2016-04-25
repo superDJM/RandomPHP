0383b2160a0d50542eed4d8b1f092cca835bd763
mz <1639632292@qq.comm>
2016-04-25 21:44:36 +0800

    模板继承

--- Library/Random/Template.php
+++ Library/Random/Template.php
@@ -20,7 +20,8 @@ class Template
 
     /** @var  array 模版变量 */
     protected $data = array();
-
+    protected $tplname = array();
+    protected $tpldata = array();
     /** @var array 模版匹配规则 */
     protected $pattern = array(
         /*普通变量
@@ -64,6 +65,9 @@ class Template
     {
         $this->dir = $dir;
         $this->com_dir = Config::get('path.TPL_TEMP_ROOT');
+        if (!is_dir($this->com_dir) && !mkdir($this->com_dir,0777,true)) {
+            throw new Exception('RandomPHP can not create the compile_dir in '.$this->com_dir);
+        }
     }
 
     /**
@@ -107,13 +111,7 @@ class Template
         if (file_exists($path)) {
             if (Config::get('debug') || !file_exists($comFile) || filemtime($comFile) < filemtime($path)) {
                 $content = file_get_contents($path);
-                $pattern = array(
-                    '/{\$([a-zA-Z]*)}/i'
-                );
-                $replacement = array(
-                    '<?php echo \$${1}; ?>'
-                );
-                $repContent = $content;
+                $repContent = $this->extendTemplate($content);
                 //模版的替换
                 /*preg_replace_callback($pattern, function ($match) use (&$repContent) {
                     $this->data[$match[1]] = empty($this->data[$match[1]]) ? '' : $this->data[$match[1]];
@@ -137,5 +135,131 @@ class Template
 
         $content = ob_get_clean();
         return $content;
+    }
+
+    /**
+     * @param string $content
+     */
+    function extendTemplate ($content = ''){
+        $isextend = $this->isextend($content);
+        //继承及block内容替换，得到父模板以及把block替换后的内容
+        if ($isextend['count']) {
+            $content    = $this->extend($content);
+        }
+        // var_dump($content);
+        //替换引用，得到引用后内容
+        $isinclude = $this->isinclude($content);
+        $include = $this->getinclude($isinclude,$content);
+        $content = preg_replace($include['includerp'],$include['includecontent'],$content);
+        // var_dump($content);
+        $content = $this->qublock($content);
+        return $content;
+    }
+
+    //继承及替换
+    function extend($content)
+    {
+        //提前获取是否继承和替换
+        $isextend = $this->isextend($content);
+        $isblock = $this->isblock($content);
+        //继承就获得继承的内容
+        if ($isextend['count']) {
+            $content = $this->getextend($isextend['extendname'][0][1],$content);
+        }
+        $blockrp = $this->getblockrp($isblock); //得到block替换的正则及替换
+        $content = preg_replace($blockrp,$isblock['blockcontent'][0],$content);
+
+        return $content;
+    }
+    function getblockrp($isblock)
+    {
+        for ($i=0; $i < count($isblock['blockname']); $i++) {
+            $blockrp[] = '/{\s*block\s*name\s*=\s*["\']'.$isblock['blockname'][$i][1].'\s*["\']\s*}.*{\s*\/\s*block\s*}/Us';
+        }
+        return $blockrp;
+    }
+    function getextend($extendname,$content)
+    {
+        $extendpath = $this->dir . '/' . $extendname ;
+
+        if (file_exists($extendpath)) {
+            $exdconp = file_get_contents($extendpath);
+            return $this->extend($exdconp);
+        }else{
+            echo "错误";
+            return null;
+        }
+
+    }
+    //获取引用内容及替换正则
+    function getinclude($isinclude,$content)
+    {
+        for ($i=0; $i < count($isinclude['includename']); $i++) {
+            $includepath = $this->dir .'/'.$isinclude['includename'][$i][1];
+            if (file_exists( $includepath)) {
+                $includecontent[] = file_get_contents($includepath);
+            }else{
+                $includecontent[] = null;
+            }
+            $includerp[] = '/{\s*include\s*name\s*=\s*[\'"].*'.$isinclude['includename'][$i][1].'[\'"]\s*}/Us';
+        }
+        return ['includecontent'=>$includecontent,'includerp'=>$includerp];
+    }
+    function isextend($content) //$extendname[0][0]==模板值
+    {
+        $content  = $this->quitnote($content);
+        $extendrp = '/{\s*extend\s*name\s*=\s*[\'"].*[\'"]\s*}/Us';
+        $count    = preg_match_all($extendrp, $content ,$extendHtml);
+        $extendname = $this->getfilename($extendHtml);
+        return ['count'=>$count,'extendname'=>$extendname];
+    }
+    function isblock($content)//$blockname[*][1];$blockcontent[0][*];
+    {
+        $content = $this->quitnote($content);
+        $blkrpl = '/{\s*block\s*name\s*=\s*[\'"].*[\'"]\s*}.*{\s*\/\s*block\s*}/Us';     //获取代码块
+        $blkrps = '/{\s*block\s*name\s*=\s*[\'"].*[\'"]\s*}/';
+        $count = preg_match_all($blkrpl, $content ,$blockcontent);
+        if ($count) {
+            preg_match_all($blkrps, $content ,$blkname);
+            $blockname=$this->getfilename($blkname);
+        }
+        return ['count'=>$count,'blockname'=>$blockname,'blockcontent'=>$blockcontent];
+    }
+    function isinclude($content) //$includename[*][0]==模板值
+    {
+        $content  = $this->quitnote($content);
+        $includerp = '/{\s*include\s*name\s*=\s*[\'"].*[\'"]\s*}/Us';
+        $count    = preg_match_all($includerp, $content ,$includeHtml);
+        $includename = $this->getfilename($includeHtml);
+        return ['count'=>$count,'includename'=>$includename];
+    }
+
+    function quitnote($content){
+        $note1 = '/\/(\*).*(\*)\//Us';   //去掉/**/
+        $note2 = '/<!--.*-->/Us';         //去掉<!-- --!>
+        $content = preg_replace($note1,null,  $content);
+        $content = preg_replace($note2,null,  $content);
+        return $content;
+    }
+    function qublock($content)
+    {
+        $blkfirst = '/{\s*block\s*name\s*=\s*["\'].*["\']\s*}/Us';
+        $blkend = '/{\s*\/\s*block\s*}/Us';
+        $content = preg_replace($blkfirst,null,$content);
+        $content = preg_replace($blkend,null,$content);
+        return  $content;
+    }
+    //获取文件名函数
+    function getfilename($isexd){
+        // var_dump($isexd);
+        if($isexd[0]){
+            for ($i=0; $i < count($isexd[0]); $i++) {
+                preg_match_all("/['\"].*['\"]/",$isexd[0][$i],$arr);
+                $arr2[] = preg_split("/['\"]/", $arr[0][0]);//去字符串''
+            }
+            return $arr2;
+        }else{
+            return null;
+        }
     }
 }
